694cf83edca3fe44405b6aa421c260a1c727ca91
Aakash Mukherjee <110044436+blackcode1996@users.noreply.github.com>
2023-06-14 12:24:19 +0530

    bug resolved

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,28 +2,40 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { useNavigate } from "react-router-dom";
 
 import { LinkContainer } from "react-router-bootstrap";
 import logo from "../Assests/logo.png";
 
 const Navigation = () => {
+
+  const navigate=useNavigate()
+
+  function handleClick(){
+    navigate("/")
+  }
+
+  function makeResume(){
+    navigate("/make_resume")
+  }
+
+
   return (
     <Navbar bg="light" expand="lg" style={{ marginBottom: "5vh" }}>
       <Container>
-        <LinkContainer to="/">
-          <Navbar.Brand>
+        
+          <Navbar.Brand onClick={handleClick}>
             <img src={logo} style={{ width: 50, height: 50 }} />
             <span style={{ marginLeft: "7px" }}>
               Make My <span style={{ color: "orange" }}>Resume</span>
             </span>
           </Navbar.Brand>
-        </LinkContainer>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <LinkContainer to="/make_resume">
-              <Nav.Link>Make Resume</Nav.Link>
-            </LinkContainer>
+            
+              <Nav.Link onClick={makeResume}>Make Resume</Nav.Link>
+            
           </Nav>
         </Navbar.Collapse>
       </Container>
