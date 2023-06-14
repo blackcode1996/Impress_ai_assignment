d01a1011d1dfc9f9c0ef42a6f33715c6e3a337bd
Aakash Mukherjee <110044436+blackcode1996@users.noreply.github.com>
2023-06-14 12:46:27 +0530

    fixed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,41 +1,28 @@
-import React from "react";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import { useNavigate } from "react-router-dom";
-
-// import { LinkContainer } from "react-router-bootstrap";
-import logo from "../Assests/logo.png";
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
+import logo from '../Assests/logo.png';
 
 const Navigation = () => {
-
-  const navigate=useNavigate()
-
-  function handleClick(){
-    navigate("/")
-  }
-
-  function makeResume(){
-    navigate("/make_resume")
-  }
-
-
   return (
-    <Navbar bg="light" expand="lg" style={{ marginBottom: "5vh" }}>
+    <Navbar bg="light" expand="lg" style={{ marginBottom: '5vh' }}>
       <Container>
-        
-          <Navbar.Brand onClick={handleClick}>
-            <img src={logo} style={{ width: 50, height: 50 }} />
-            <span style={{ marginLeft: "7px" }}>
-              Make My <span style={{ color: "orange" }}>Resume</span>
+        <Link to="/" style={{textDecoration:"none"}}>
+          <Navbar.Brand>
+            <img src={logo} style={{ width: 50, height: 50 }} alt="Logo" />
+            <span style={{ marginLeft: '7px' }}>
+              Make My <span style={{ color: 'orange' }}>Resume</span>
             </span>
           </Navbar.Brand>
+        </Link>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            
-              <Nav.Link onClick={makeResume}>Make Resume</Nav.Link>
-            
+            <Link to="/make_resume" className="nav-link">
+              Make Resume
+            </Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
